abf2a9ed35e2d8c226783f0a2708e38cc1bf4005
Cenngo <cenk.ergen1@gmail.com>
2022-11-19 22:56:14 +0300

    json tool impl

---
+++ Discord.Net.Tools.Localization.Json/Entry.cs
@@ -0,0 +1,55 @@
+﻿using Discord.Rest;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Discord.Net.Tools.Localization.Json
+{
+    [JsonObject(MissingMemberHandling = MissingMemberHandling.Ignore, ItemNullValueHandling = NullValueHandling.Ignore)]
+    public record Entry
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonExtensionData(WriteData = true)]
+        public IDictionary<string, object> Entries { get; set; }
+    }
+
+    public static class EntryExtensions
+    {
+        public static Entry ToEntry(this RestApplicationCommand command, bool skipEmpty)
+        {
+            return new()
+            {
+                Name = command.Name,
+                Description = string.IsNullOrEmpty(command.Description) && skipEmpty ? null : command.Description,
+                Entries = command.Options.ToDictionary(x => x.Name, x => x.ToEntry() as object)
+            };
+        }
+
+        public static Entry ToEntry(this RestApplicationCommandOption option)
+        {
+            IDictionary<string, object>? subEntries = option.Type switch
+            {
+                ApplicationCommandOptionType.SubCommand or ApplicationCommandOptionType.SubCommandGroup =>
+                    option.Options?.ToDictionary(x => x.Name, x => x.ToEntry() as object),
+                _ => option.Choices?.ToDictionary(x => x.Name, x => x.ToEntry() as object)
+            };
+
+            return new()
+            {
+                Name = option.Name,
+                Description = option.Description,
+                Entries = subEntries!
+            };
+        }
+
+        public static Entry ToEntry(this RestApplicationCommandChoice choice) =>
+            new Entry
+            {
+                Name = choice.Name
+            };
+    }
+}
--- Discord.Net.Tools.Localization.Json/Program.cs
+++ Discord.Net.Tools.Localization.Json/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord.Net.Tools.Localization;
+using Discord.Net.Tools.Localization.Common;
+using Discord.Net.Tools.Localization.Json;
+using Discord.Rest;
+using Newtonsoft.Json;
+using Serilog;
+using System.Text;
+
+await Utils.RunAsync(args, ProcessCommands);
+
+async Task ProcessCommands(IReadOnlyCollection<RestApplicationCommand> commands, Options options)
+{
+    var outputFilePath = Path.Combine(options.OutputDir, options.FileName + ".json");
+    if (Path.GetExtension(outputFilePath) != ".json")
+        throw new ArgumentException("Output file extesion must be '.json'");
+
+    Log.Information($"Initializing, using {outputFilePath} as output.");
+
+    using var sw = new StreamWriter(outputFilePath);
+    var entries = commands.ToDictionary(x => x.Name, x => x.ToEntry(options.SkipNullOrEmpty));
+
+    Log.Information($"Serializing JSON object with {entries.Count} entries.");
+
+    var json = JsonConvert.SerializeObject(entries, Formatting.Indented);
+
+    Log.Information($"Writing the JSON file.");
+
+    await sw.WriteAsync(json);
+}
